2269c554339d4d5c901def235cbe294bf1965211
Darya Fesenko <darya.dobrafesenko@mail.ru>
2021-10-03 12:36:29 +0300

    fix port

--- cmd/url_shortener/main.go
+++ cmd/url_shortener/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 	"urlshortener/app/starter"
 	"urlshortener/db/pgsql"
 )
 
 func main() {
-	os.Setenv("PG_DSN", "postgres://postgres:@localhost/shortener?sslmode=disable")
-	os.Setenv("PORT", "9000")
+	//os.Setenv("PG_DSN", "postgres://postgres:@localhost/shortener?sslmode=disable")
+	//os.Setenv("PORT", "9000")
 	app, err := starter.NewApp()
 	if err != nil {
 		log.Fatal(err)
